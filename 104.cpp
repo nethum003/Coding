648f2d476003ac0a3de55a163612c569dd9c9d04
nethum003 <sandiwnethum@gmail.com>
2024-11-04 19:56:26 +0530

    Name & Age 3
    Print personalized text according to user input

---
+++ 104.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    string name;
+    int age;
+    cout<<"Enter your name"<<endl;
+    cin>>name;
+    cout<<"Enter your age"<<endl;
+    cin>>age;
+    cout<<"Hello "<<name<<endl;
+    cout<<"Your age is "<<age;
+
+    return 0;
+}
