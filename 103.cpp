4256f158fec727f018936db29855178842ff0e1f
nethum003 <sandiwnethum@gmail.com>
2024-11-04 19:54:22 +0530

    Name  & Age 2
    Enter users name and age using cin tag

---
+++ 103.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    string name;
+    int age;
+    cout<<"Enter Your Name: "<<endl;
+    cin>>name;
+    cout<<"Enter Your age"<<endl;
+    cin>>age;
+
+    return 0;
+}
